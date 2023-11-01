cfa2adf33d9c5d507735bd9d371c162473a1dd80
Allan Khariton <allankhariton@gmail.com>
2023-10-31 22:53:59 -0400

    removing problematic graph

--- streamlit-web-app-ducks.py
+++ streamlit-web-app-ducks.py
@@ -39,7 +39,7 @@ yearly_df = df.groupby(["Year"]).agg({"Quantity":"sum"}).reset_index()
 
 weight_df = df.groupby(["Year"]).agg({"Total_Weight":"sum"}).reset_index()
 
-weight_cum_df = df.groupby(['Year']).sum().cumsum().reset_index()
+# weight_cum_df = df.groupby(['Year']).sum().cumsum().reset_index()
 
 ## insert a title for the app and instructions
 
